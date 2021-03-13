98efdb632234b504298391b1b4f9df4b6e2fd7ab
JosueXA <josue.xicotencal.a@gmail.com>
2021-03-13 15:55:14 -0600

    optimization code in TodoApp component childrens

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from 'react';
 import { useForm } from '../../hooks/useForm';
 import { todoReducer } from '../07-tarea-memo/todoReducer';
+import { TodoList } from './TodoList';
 
 import './styles.css';
 
@@ -35,8 +36,17 @@ export const TodoApp = () => {
       type: 'delete',
       payload: todoId
     }
-    //dispatch
+
     dispatch( action );
+
+  }
+
+  const handleToggle = ( todoId ) => {
+
+    dispatch({
+      type: 'toggle',
+      payload: todoId
+    });
 
   }
 
@@ -75,26 +85,11 @@ export const TodoApp = () => {
 
         <div className="col-7">
 
-          <ul className="list-group list-group-flush">
-            {
-              todos.map( (todo, i) => {
-                return (
-                  <li
-                    key={ todo.id }
-                    className="list-group-item"
-                  >
-                    <p className="text-center"> { i + 1 }. {todo.desc} </p>
-                    <button 
-                      className="btn btn-danger"
-                      onClick={ () => handleDelete( todo.id ) }
-                    >
-                      Borrar
-                    </button>
-                  </li>
-                )
-              })
-            }
-          </ul>
+          <TodoList 
+            todos={ todos } 
+            handleDelete={ handleDelete } 
+            handleToggle={ handleToggle } 
+          />
 
         </div>
 
